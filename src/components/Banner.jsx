cd127500bf6f6f6ce486e46ca4a86d03619ddd8b
mahamudul hasan seber <sebermh@gmail.com>
2024-10-24 00:12:03 +0600

    add code

---
+++ src/components/Banner.jsx
@@ -0,0 +1,19 @@
+
+const Banner = () => {
+    return (
+        <div className="">
+            <div className="hero-content text-center">
+                <div className="max-w-md">
+                    <h1 className="mb-5 text-5xl font-bold">Discover an exceptional cooking class tailored for you!</h1>
+                    <p className="mb-5">
+                        Learn and Master Basic Programming, Data Structures, Algorithm, OOP, Database and solve 500+ coding problems to become an exceptionally well world-class Programmer.
+                    </p>
+                    <button className="btn bg-green-400 mr-7">Explore Now</button>
+                    <button className="btn ">Our Feedback</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+
+const Header = () => {
+    return (
+        <div>
+            <div className="navbar bg-base-100">
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor">
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h8m-8 6h16" />
+                            </svg>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                            <li><a>Home</a></li>
+                            <li><a>Recipes</a></li>
+                            <li><a>About</a></li>
+                            <li><a>Search</a></li>
+                        </ul>
+                    </div>
+                    <a className="btn btn-ghost font-extrabold text-2xl">Recipe Calories</a>
+                </div>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1 font-bold">
+                        <li><a>Home</a></li>
+                        <li><a>Recipes</a></li>
+                        <li><a>About</a></li>
+                        <li><a>Search</a></li>
+                    </ul>
+                </div>
+                <div className="navbar-end">
+
+                    <input className="border border-[1px solid bg-red-100] p-2 rounded-md mr-10" type="text" placeholder="Search" />
+                    <a className=" btn bg-green-300 rounded-full"><i className="fa-regular fa-user"></i></a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
